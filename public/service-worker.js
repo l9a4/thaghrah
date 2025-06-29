7589f3647b7db2636cb44dc4f36ed63da13866fc
l9a4 <80963307+l9a4@users.noreply.github.com>
2025-06-29 13:06:21 +0300

    Add PWA support and color scheme

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,24 @@
-service-worker.js
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('thaghrah-v1').then(cache =>
-      cache.addAll(['/', '/css/style.css', '/assets/cyber-bg.mp4'])
+      cache.addAll([
+        '/',
+        '/css/style.css',
+        '/js/theme.js',
+        '/js/menu.js',
+        '/js/auth-form.js',
+        '/js/sw-register.js',
+        '/assets/cyber-bg.mp4'
+      ])
+    )
+  );
+});
+self.addEventListener('activate', event => {
+  const current = 'thaghrah-v1';
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== current).map(k => caches.delete(k)))
     )
   );
 });
